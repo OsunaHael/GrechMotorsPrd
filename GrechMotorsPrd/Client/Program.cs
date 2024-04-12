41e0d8b4027ca294032b8264e627f9e8a32634ba
OsunaHael <osunahael@gmail.com>
2024-04-12 14:31:02 -0700

    Creacion de metodos para la generacion de codigos QR e implementacion de descarga de archivos PDF de los codigos QR

--- GrechMotorsPrd/Client/Program.cs
+++ GrechMotorsPrd/Client/Program.cs
@@ -1,11 +1,18 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using GrechMotorsPrd.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using QRCoder;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Registra QrCodeGenerator como un servicio
+builder.Services.AddSingleton<QrCodeGenerator>();
+
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
