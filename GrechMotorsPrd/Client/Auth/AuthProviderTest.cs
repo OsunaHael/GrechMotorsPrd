50cc4a7597f344d6ec8c400ae2b6e05aa9b069cd
osunahael <osunahael@gmail.com>
2024-04-24 16:57:17 -0700

    Se agregaron unas configuraciones para empezar a hacer lo de la autenticacion

---
+++ GrechMotorsPrd/Client/Auth/AuthProviderTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace GrechMotorsPrd.Client.Auth
+{
+    public class AuthProviderTest : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonimo = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonimo)));
+        }
+    }
+}
--- GrechMotorsPrd/Client/Program.cs
+++ GrechMotorsPrd/Client/Program.cs
@@ -1,5 +1,7 @@
 using CurrieTechnologies.Razor.SweetAlert2;
 using GrechMotorsPrd.Client;
+using GrechMotorsPrd.Client.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using QRCoder;
@@ -8,11 +10,17 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+ConfigureServices(builder.Services);
 
 // Registra QrCodeGenerator como un servicio
 builder.Services.AddSingleton<QrCodeGenerator>();
 
-builder.Services.AddSweetAlert2();
-
 await builder.Build().RunAsync();
+
+void ConfigureServices(IServiceCollection services)
+{
+    services.AddSweetAlert2();
+    services.AddAuthorizationCore();
+    services.AddScoped<AuthenticationStateProvider, AuthProviderTest>();
+}
